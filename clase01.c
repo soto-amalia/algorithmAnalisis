a649c99ce3155a43a2000fc8fa8109fb6aa5c689
AmaliaLinux <sotoes.amalia@gmail.com>
2025-02-24 20:59:11 -0600

    clase 1 y 2

--- clase01.c
+++ clase01.c
@@ -17,4 +17,52 @@ Criterios:
 Eficientes de tiempo, almacenamiento, la cantidad de trafico que fgenera en la red, la cantidad de datos hacia siaco
 ANalilsis:Indentificar la complejidad de diferentes algoritmos para resolver un mismo porblema
 DIseño: Reconocen diferentes estrategias para resolver el problema
+
+Revisión histórica 
+MODELOS FORMALES DE COMPUTO(1936)
+-ALan Turing:Maquina de Turing
+ALonso Chrch:Calculo Lamsa
+CLaude SHaron : Buleana a la modelacion de circuitos
+-Teoria matematicade la comunicación
+Primeras compus
+-Arquitectura Von Neuman
+-Z3 Y ENIAC
+Primeros lenguajes
+-Fortran, COBOL Y LISP
+Lenguaje de google---> 
+Dónde procesamos cadenas? 
+Máquinas de estados finitos 
+Rabin y Scott Herramientos para el estudio de las capacidades y limitaciones de las maquinas de 
+esstados finitos y automatas(aspiradora, labadora, chatgpt) de pila
+Lenguajes Formales
+-Noam Chomsky: La jerarquia de lenguajes
+60's
+COmplejidad COmputacional
+-Clasificación de funciones en tiempo y espacio:Complegidad Computacional(Chatgpt o(n)BUsqueda y Recuperación, n cuadrada en entrenamiento,
+temporal-O(nxd) o o(nlogn), o(1)- para tareas simples )
+y existen otras como O(N CUBICA), o(N!) Y O(n exponencial n)
+-Jearquias de problemas:P y NP
+cOOK Y lEVINE:pROBLEMAS NP complestos
+-Conjetura P=NP
+70's
+Tiempo de cómputo y complejidad de circuitos
+80's
+Procesamiento paralelo y distribuido 
+-MOdelos para evaluar la comjidad de entrada/salida de algoritmos
+90's
+Criptografia
+2000's
+Metosods de aproximación
+Metodos de optimizació combinatoria
+-CoMPUTACIÓN EVOLUTIVA
+-COmputación cuantica
+NO determinismo 
+KNN-ALgoritmos importantes
+    Arboles de desición
+    Regresion LIneal, Regresion LOgistica, Redes neuronales profundas, confundicionales , Forest, Alton Coles, Redes condicionales, Baye
+C
+Procesadores de lenguaje natural, transformadores(chatgpt), CUantico, es mas rapido y da una respuesta simultania, requiere mas recursos computacionales y de manera eficiente
+Insercion, busqueda binaria
+
+
 )*/
